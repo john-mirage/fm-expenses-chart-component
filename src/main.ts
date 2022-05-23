b52d350221c26f459fdd050d2f2b92ba91de2504
john-mirage <chaosdynamix@posteo.net>
2022-05-23 15:19:23 +0200

    feat: finish and deploy

--- src/main.ts
+++ src/main.ts
@@ -10,6 +10,17 @@ interface ChartBarsProps {
   bars: Bar[];
 }
 
+const barProgressAnimation: Keyframe[] = [
+  { transform: 'scaleY(0)' },
+  { transform: 'scaleY(1)' }
+];
+
+const barProgressTiming: KeyframeAnimationOptions = {
+  duration: 300,
+  iterations: 1,
+  fill: "forwards"
+}
+
 class ChartBars extends HTMLElement {
   bars: Bar[] | false;
 
@@ -18,17 +29,26 @@ class ChartBars extends HTMLElement {
     this.bars = false;
   }
 
-  createBar(bar, maxAmount) {
+  createBar(bar, index, maxAmount) {
     const height = String(Math.round((bar.amount * 100) / maxAmount));
     const template = document.getElementById("template-bar") as HTMLTemplateElement;
     const fragment = template.content.cloneNode(true) as DocumentFragment;
     const chartBar = fragment.querySelector(".bar") as HTMLDivElement;
     const chartBarProgress = fragment.querySelector(".bar__progress") as HTMLDivElement;
     const chartBarDay = fragment.querySelector(".bar__day") as HTMLParagraphElement;
+    const chartBarAmount = fragment.querySelector(".bar__amount") as HTMLParagraphElement;
     chartBarDay.textContent = bar.day;
+    chartBarAmount.textContent = `$${String(bar.amount)}`;
     chartBar.classList.add(bar.amount === maxAmount ? "bar--cyan" : "bar--red");
     chartBarProgress.style.height = `${height}%`;
     this.appendChild(chartBar);
+    if (index > 0) {
+      setTimeout(() => {
+        chartBarProgress.animate(barProgressAnimation, barProgressTiming);
+      }, index * 300);
+    } else {
+      chartBarProgress.animate(barProgressAnimation, barProgressTiming);
+    }
   }
 
   connectedCallback() {
@@ -37,10 +57,10 @@ class ChartBars extends HTMLElement {
       const maxAmount = Math.max(...barAmounts);
       const gridItems = String(this.bars.length);
       this.style.gridTemplateColumns = `repeat(${gridItems}, 1fr)`;
-      this.bars.forEach((bar) => this.createBar(bar, maxAmount));
       this.classList.add("chart__bars");
+      this.bars.forEach((bar, index) => this.createBar(bar, index, maxAmount));
     } else {
-      console.warn("The chart bars web components has no bars");
+      console.warn("The chart bars web component has no bars");
     }
   }
 }
